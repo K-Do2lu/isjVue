1f8715c525d859a3985bb38132d2f26b86ff1e77
kdy <kdy992m@naver.com>
2024-06-25 18:00:25 +0900

    도야호

--- src/components/areaHeader.vue
+++ src/components/areaHeader.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>rlahEl</div>
+  <header>
+    <div class="h-inner">
+      <a href="#" class="h-logo"></a>
+
+      <nav>
+        <ul>
+          <li v-for="(li, index) in menu" :key="index">
+            <a href="#" class="m-tit">{{ li.tit }}</a>
+
+            <ul class="m-sub">
+              <li>
+                <a href="#" class="m-sub-tit">ART&DESIGN</a>
+
+                <ul>
+                  <li>제품 ∙ 시스템 디자인</li>
+                  <li>환경 ∙ 공공 디자인</li>
+                  <li>대학 디자인</li>
+                </ul>
+              </li>
+              <li>
+                <a href="#" class="m-sub-tit">RESEARCH</a>
+
+                <ul>
+                  <li>국가디자인 ∙ 교육정책</li>
+                  <li>미래 라이프스타일 ∙ 디자인전략</li>
+                  <li>미래도시 ∙ 모빌리티 ∙ 모바일 디자인</li>
+                  <li>정체성 ∙ 혁신 ∙ 한국디자인</li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+        <div class="nav-btn">
+          <button type="button" class="icon search"></button>
+          <button type="button" class="icon menu"></button>
+        </div>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -9,7 +48,15 @@ export default {
     msg: String,
   },
   data() {
-    return {};
+    return {
+      menu: [
+        { tit: "HOME" },
+        { tit: "ABOUT" },
+        { tit: "WORK" },
+        { tit: "EVENT" },
+        { tit: "MEDIA" },
+      ],
+    };
   },
   methods: {},
 };
