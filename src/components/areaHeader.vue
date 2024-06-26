fb9a392943ce1c006c9b54a4162acef7fdfc0336
kdy <kdy992m@naver.com>
2024-06-26 17:39:20 +0900

    header

--- src/components/areaHeader.vue
+++ src/components/areaHeader.vue
@@ -5,27 +5,27 @@
 
       <nav>
         <ul>
-          <li v-for="(li, index) in menu" :key="index">
+          <li
+            v-for="(li, index) in menu"
+            :key="index"
+            :class="{ clicked: li.clicked === true }"
+            @click="li.clicked === true"
+          >
             <a href="#" class="m-tit">{{ li.tit }}</a>
 
-            <ul class="m-sub">
-              <li>
-                <a href="#" class="m-sub-tit">ART&DESIGN</a>
+            <ul class="m-sub" v-if="li.subs">
+              <li
+                v-for="(sub, index) in li.subs"
+                :key="index"
+                :class="{ active: sub.active }"
+                @click="subToggle(sub)"
+              >
+                <a href="#" class="m-sub-tit">{{ sub.tit }}</a>
 
-                <ul>
-                  <li>제품 ∙ 시스템 디자인</li>
-                  <li>환경 ∙ 공공 디자인</li>
-                  <li>대학 디자인</li>
-                </ul>
-              </li>
-              <li>
-                <a href="#" class="m-sub-tit">RESEARCH</a>
-
-                <ul>
-                  <li>국가디자인 ∙ 교육정책</li>
-                  <li>미래 라이프스타일 ∙ 디자인전략</li>
-                  <li>미래도시 ∙ 모빌리티 ∙ 모바일 디자인</li>
-                  <li>정체성 ∙ 혁신 ∙ 한국디자인</li>
+                <ul v-if="sub.item">
+                  <li v-for="(item, index) in sub.item" :key="index">
+                    <a href="#">{{ item.tit }}</a>
+                  </li>
                 </ul>
               </li>
             </ul>
@@ -50,16 +50,96 @@ export default {
   data() {
     return {
       menu: [
-        { tit: "HOME" },
-        { tit: "ABOUT" },
-        { tit: "WORK" },
-        { tit: "EVENT" },
-        { tit: "MEDIA" },
+        { tit: "HOME", clicked: true },
+        { tit: "ABOUT", clicked: false },
+        {
+          tit: "WORK",
+          clicked: false,
+          subs: [
+            {
+              tit: "ART&DESIGN",
+              item: [
+                { tit: "제품 ∙ 시스템 디자인" },
+                { tit: "환경 ∙ 공공 디자인" },
+                { tit: "대학 디자인" },
+              ],
+              active: false,
+            },
+            {
+              tit: "RESEARCH",
+              item: [
+                { tit: "국가디자인 ∙ 교육정책" },
+                { tit: "미래 라이프스타일 ∙ 디자인전략" },
+                { tit: "미래도시 ∙ 모빌리티 ∙ 모바일 디자인" },
+                { tit: "정체성 ∙ 혁신 ∙ 한국디자인" },
+              ],
+              active: false,
+            },
+          ],
+        },
+        {
+          tit: "EVENT",
+          clicked: false,
+          subs: [
+            {
+              tit: "EXHIBITION",
+              item: [{ tit: "개인전" }, { tit: "참여전시" }],
+              active: false,
+            },
+            {
+              tit: "PROGRAM PLANNING",
+              item: [{ tit: "행사 참여작품" }],
+              active: false,
+            },
+          ],
+        },
+        {
+          tit: "MEDIA",
+          clicked: false,
+          subs: [
+            {
+              tit: "LECTURES",
+              item: [{ tit: "주제 특강" }, { tit: "강의" }],
+              active: false,
+            },
+            {
+              tit: "BOOKS",
+              item: [{ tit: "Books" }, { tit: "Article" }],
+              active: false,
+            },
+            {
+              tit: "PRESS",
+              item: [{ tit: "Interview" }, { tit: "News" }, { tit: "Video" }],
+              active: false,
+            },
+          ],
+        },
       ],
     };
   },
-  methods: {},
+  methods: {
+    subToggle(sub) {
+      sub.active = !sub.active;
+
+      this.menu.forEach((li) => {
+        if (li.subs) {
+          li.subs.forEach((item) => {
+            if (item !== sub) {
+              item.active = false;
+            }
+          });
+        }
+      });
+    },
+  },
 };
+
+// m-sub li의 class가 active라면
+// m-sub의 display: block
+
+document.addEventListener("click", (e) => {
+  console.log(e.target);
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
